7761cf3d8741a721b98893052f961687d1f8be6d
anojnuclei <anoj@nuclei.tech>
2025-11-28 11:03:19 +0530

    feat: add inventory item model, CORS allowed origins, and attendance session management

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,7 @@
 const allowedOrigins = [
   "https://ananda-hostel-web.vercel.app",
   "http://localhost:3000",
-  "http://localhost:5173",
   "http://52.136.38.68:5000",
   "http://localhost:5173",
-  "https://ananda-hostal-nuoxhfpvc-it21161056s-projects.vercel.app",
 ];
 export default allowedOrigins;
