c86f5c1d62444c66ff6f0b96dd27cf670b6a19b6
Anoj Peiris <99235396+IT21161056@users.noreply.github.com>
2026-02-13 22:44:30 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
   "https://ananda-hostel-web.vercel.app",
+  "https://ananda-hostel-web-ruby.vercel.app",
+  "https://ananda-hostel-erx8cqdhf-anojs-projects-0637f240.vercel.app",
   "http://localhost:3000",
   "http://52.136.38.68:5000",
   "http://localhost:5173",
